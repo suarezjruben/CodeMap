9276874c7c9a96877a0940184587fea2d15b5d47
Ruben <suarezjruben@gmail.com>
2022-01-19 09:18:17 -0600

    Deleted comments

--- app.py
+++ app.py
@@ -8,19 +8,20 @@ app.static_folder = 'static'
 
 
 @app.route('/')
-def index():  # put application's code here
+def index():  
     return flask.render_template("index.html")
+
 
 @app.route('/Main')
 def search():
     return flask.render_template("Main.html")
 
 
-@app.route('/hello',methods=["GET", "POST"])
+@app.route('/hello', methods=["GET", "POST"])
 def hello():
     if request.method == 'POST':
-        print("Recieved")
-        data = (request.get_json()) #parse as Json
+        print("Received")
+        data = (request.get_json())  # parse as Json
         with open('static/newSearch.json', 'w') as json_file:
             json.dump(data, json_file)
         return 'OK', 200
